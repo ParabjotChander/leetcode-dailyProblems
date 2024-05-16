459ae9ec3da701eb5b4be0cb8ab86548b559e1b9
Parabjot Chander <parabjotchander23@gmail.com>
2024-05-16 12:23:55 -0400

    may 16,2024 question added

---
+++ problems/leetcode2331.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public boolean evaluateTree(TreeNode root) {
+
+        // base case if root node is a leaf we return true or false based on leaf int value
+        if(root.left == null && root.right == null){
+            return root.val == 1 ? true : false;
+        }
+
+        // if non leaf node is OR we do || recursively literate through the right and left subtree
+        return root.val == 2 ? evaluateTree(root.left) || evaluateTree(root.right) : evaluateTree(root.left) && evaluateTree(root.right);
+    }
+
+}
